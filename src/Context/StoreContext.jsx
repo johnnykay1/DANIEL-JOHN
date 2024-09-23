c1d4ee10cf71dbd061d56cc880470085fd8bb2f2
johnnykay1 <johnnykay1232@gmail.com>
2024-09-22 22:58:59 -1100

    john

---
+++ src/Context/StoreContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+
+
+
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+
+    const [cartItems,setcartItems] = useState({})
+
+    const addToCart = (itemId) =>{
+        if (!cartItems[itemId]) {
+            setcartItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else {
+            setcartItems((prev)=>({...prev,[itemId]:prev[itemId+1]}))
+        }
+    }
+
+    const removeFromcart = (itemId) => {
+        setcartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    useEffect(()=> {
+         console.log(cartItems);
+         
+    },[cartItems])
+
+    const contextValue ={
+        food_list,
+        cartItems,
+        setcartItems,
+        addToCart,
+        removeFromcart,
+    }
+
+    
+    return (
+        <StoreContext.Provider value={contextValue}>
+             {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider
